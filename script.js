af63145bae7826d74ccc1867c23777100491f93e
Cristian1225 <cristianoreyes234@gmail.com>
2023-10-02 21:20:00 -0700

    updated index

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define variables
+let firstName = "Cristian";
+let lastName = "Reyes Chavez";
+let yearsOfStudy = " for the past 7 months.";
+let goal = "I plan to Become a Front End Software Developer";
